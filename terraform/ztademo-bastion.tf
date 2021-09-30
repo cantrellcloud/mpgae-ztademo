1351c92965fc5045a9236f021716f04272216f6e
Ron Cantrell <ron.cantrell@cantrelloffice.cloud>
2021-09-29 22:43:40 -0400

    updated subnet and bastion

---
+++ terraform/ztademo-bastion.tf
@@ -0,0 +1,63 @@
+#================================================================================
+#
+# ZTADEMO
+# Bastion Host
+#
+#================================================================================
+#
+# BASTION HOST and PUBLIC IPs
+#
+# bastion01
+module "ztademo_eastus2_dev_ztadev_bast01_bastion" {
+	source = "github.com/cantrellcloud/tfAzureModules/bastions"
+	#to_provision = local.provision_bastion01
+	rg_location = module.ztademo_eastus2_dev_rg.rg_location
+	rg_name = module.ztademo_eastus2_dev_rg.rg_name
+	rg_tags = {
+		"ManagementGroup" = "MPG A&E EastUS2",
+		"Environment" = "Demo",
+		"AutomatedBy" = "Terraform",
+		"Note1" = "Do not manually change",
+		"POCName" = "ronc@mindpointgroup.com",
+		"POCPhone" = "843.330.6769",
+		"Project" = "Zero Trust Demo"
+	}
+
+	bastions_name = "ztadev-bast01"
+    ip_configuration_name                 = "ztadev_bast01_ip"
+    ip_configuration_subnet_id            = module.ztademo_eastus2_azure_bastion_subnet.subnet_id
+    ip_configuration_public_ip_address_id = module.ztademo_eastus2_dev_ztadev_bast01_publicip.publicip_id
+}
+
+    # ztadev_bast01_publicip
+    module "ztademo_eastus2_dev_ztadev_bast01_publicip" {
+        source            = "github.com/cantrellcloud/tfAzureModules/publicip"
+        #to_provision     = local.provision_ztadev_bast01_publicip
+        rg_location       = module.ztademo_eastus2_dev_rg.rg_location
+        rg_name           = module.ztademo_eastus2_dev_rg.rg_name
+        publicip_name     = "ztademo_eastus2_dev_ztadev_bast01_publicip"
+        allocation_method = "Static"
+        sku               = "Standard"
+        domain_name_label = "ztadevbast01"
+    }
+
+    # ztadev_bast01_outputs
+    output "ztademo_eastus2_dev_ztadev_bast01_bastion_id" {
+        value = module.ztademo_eastus2_dev_ztadev_bast01_bastion.bastions_id
+    }
+    output "ztademo_eastus2_dev_ztadev_bast01_bastion_name" {
+        value = module.ztademo_eastus2_dev_ztadev_bast01_bastion.bastions_name
+    }
+    output "ztademo_eastus2_dev_ztadev_bast01_bastion_dns_name" {
+        value = module.ztademo_eastus2_dev_ztadev_bast01_bastion.bastions_dns_name
+    }
+	#ztadev_bast01_publicip_outputs
+    output "ztademo_eastus2_dev_ztadev_bast01_publicip_id" {
+        value = module.ztademo_eastus2_dev_ztadev_bast01_publicip.publicip_id
+    }
+    output "ztademo_eastus2_dev_ztadev_bast01_publicip_name" {
+        value = module.ztademo_eastus2_dev_ztadev_bast01_publicip.publicip_name
+    }
+    output "ztademo_eastus2_dev_ztadev_bast01_publicip_domain_name_label" {
+        value = module.ztademo_eastus2_dev_ztadev_bast01_publicip.publicip_domain_name_label
+    }
--- terraform/ztademo-subnet.tf
+++ terraform/ztademo-subnet.tf
@@ -15,6 +15,14 @@ module "ztademo_eastus2_dev_prod_subnet" {
 	vnet_name = module.ztademo_eastus2_dev_vnet.vnet_name
 	subnet_prefixes = ["172.16.200.0/27"]
 }
+	#subnet01
+		output "ztademo_eastus2_dev_prod_subnet_id" {
+			value = module.ztademo_eastus2_dev_prod_subnet.subnet_id
+		}
+		output "ztademo_eastus2_dev_prod_subnet_name" {
+			value = module.ztademo_eastus2_dev_prod_subnet.subnet_name
+		}
+
 # subnet02
 module "ztademo_eastus2_dev_subnetgw_subnet" {
 	source = "github.com/cantrellcloud/tfAzureModules/subnet"
@@ -24,6 +32,14 @@ module "ztademo_eastus2_dev_subnetgw_subnet" {
 	vnet_name = module.ztademo_eastus2_dev_vnet.vnet_name
 	subnet_prefixes = ["172.16.200.248/29"]
 }
+
+		#subnet02
+		output "ztademo_eastus2_dev_subnetgw_subnet_id" {
+			value = module.ztademo_eastus2_dev_subnetgw_subnet.subnet_id
+		}
+		output "ztademo_eastus2_dev_subnetgw_subnet_name" {
+			value = module.ztademo_eastus2_dev_subnetgw_subnet.subnet_name
+		}
 
 # subnet03
 module "ztademo_eastus2_dev_ztcontroller_subnet" {
@@ -35,6 +51,14 @@ module "ztademo_eastus2_dev_ztcontroller_subnet" {
 	subnet_prefixes = ["172.16.200.32/27"]
 }
 
+		#subnet03
+		output "ztademo_eastus2_dev_ztcontroller_subnet_id" {
+			value = module.ztademo_eastus2_dev_ztcontroller_subnet.subnet_id
+		}
+		output "ztademo_eastus2_dev_ztcontroller_subnet_name" {
+			value = module.ztademo_eastus2_dev_ztcontroller_subnet.subnet_name
+		}
+
 # subnet04
 module "ztademo_eastus2_dev_edge_subnet" {
 	source = "github.com/cantrellcloud/tfAzureModules/subnet"
@@ -44,34 +68,27 @@ module "ztademo_eastus2_dev_edge_subnet" {
 	vnet_name = module.ztademo_eastus2_dev_vnet.vnet_name
 	subnet_prefixes = ["172.16.200.64/27"]
 }
-	#subnet01
-		output "ztademo_eastus2_dev_prod_subnet_id" {
-			value = module.ztademo_eastus2_dev_prod_subnet.subnet_id
-		}
-		output "ztademo_eastus2_dev_prod_subnet_name" {
-			value = module.ztademo_eastus2_dev_prod_subnet.subnet_name
-		}
-
-		#subnet02
-		output "ztademo_eastus2_dev_subnetgw_subnet_id" {
-			value = module.ztademo_eastus2_dev_subnetgw_subnet.subnet_id
-		}
-		output "ztademo_eastus2_dev_subnetgw_subnet_name" {
-			value = module.ztademo_eastus2_dev_subnetgw_subnet.subnet_name
-		}
-
-		#subnet03
-		output "ztademo_eastus2_dev_ztcontroller_subnet_id" {
-			value = module.ztademo_eastus2_dev_ztcontroller_subnet.subnet_id
-		}
-		output "ztademo_eastus2_dev_ztcontroller_subnet_name" {
-			value = module.ztademo_eastus2_dev_ztcontroller_subnet.subnet_name
-		}
-
 		#subnet04
 		output "ztademo_eastus2_dev_edge_subnet_id" {
 			value = module.ztademo_eastus2_dev_edge_subnet.subnet_id
 		}
 		output "ztademo_eastus2_dev_edge_subnet_name" {
 			value = module.ztademo_eastus2_dev_edge_subnet.subnet_name
+		}
+
+# subnet05
+module "ztademo_eastus2_azure_bastion_subnet" {
+	source = "github.com/cantrellcloud/tfAzureModules/subnet"
+	#to_provision = local.provision_subnet05
+	rg_name = module.ztademo_eastus2_dev_rg.rg_name
+	subnet_name = "AzureBastionSubnet"
+	vnet_name = module.ztademo_eastus2_dev_vnet.vnet_name
+	subnet_prefixes = ["172.16.200.96/27"]
+}
+		#subnet05
+		output "ztademo_eastus2_azure_bastion_subnet_id" {
+			value = module.ztademo_eastus2_azure_bastion_subnet.subnet_id
+		}
+		output "ztademo_eastus2_azure_bastion_subnet_name" {
+			value = module.ztademo_eastus2_azure_bastion_subnet.subnet_name
 		}
