be13f53c4e4c31d88d213dc640330b11ec960e01
Ron Cantrell <ron.cantrell@cantrelloffice.cloud>
2022-06-21 11:24:07 -0400

    added jira server

--- terraform/jidcjto-compute-vmwin.tf
+++ terraform/jidcjto-compute-vmwin.tf
@@ -1204,3 +1204,101 @@ module "jtodev_westus2_dev_jtodevwks10_vmwin" {
 
 
 #########################################################################################
+
+# jtodevjirsd01
+module "jtodev_eastus2_dev_jtodevjirsd01_vmwin" {
+	source = "github.com/cantrellcloud/tfaz-vmwin"
+	#to_provision = local.provision_jtodevjirsd01
+	rg_location = module.jtodev_eastus2_dev_rg.rg_location
+	rg_name = module.jtodev_eastus2_dev_rg.rg_name
+	rg_tags = {
+		"ManagementGroup" = "MPG A&E EastUS2",
+		"Environment" = "JTODev",
+		"AutomatedBy" = "Terraform",
+		"Note1" = "Do not manually change",
+		"POCName" = "ronc@mindpointgroup.com",
+		"POCPhone" = "843.330.6769",
+		"Project" = "Zero Trust Demo"
+	}
+
+	vmwin_name = "jtodev-jirsd01"
+    size                  = "Standard_D4s_v3"
+    admin_username        = "adminlocal"
+    admin_password        = "Password#1Password#1"
+
+    computer_name         = "jtodev-jirsd01"
+    license_type          = "Windows_Server"
+    network_interface_ids = [
+        module.jtodev_eastus2_dev_jtodevjirsd01_netinf.netinf_id,
+    ]
+
+    os_disk_caching              = "ReadWrite"
+    os_disk_storage_account_type = "Standard_LRS"
+
+    source_image_reference_publisher = "MicrosoftWindowsServer"
+    source_image_reference_offer     = "WindowsServer"
+    source_image_reference_sku       = "2019-Datacenter"
+    #version   = "latest"
+}
+
+   # jtodevjirsd01_netinf
+    module "jtodev_eastus2_dev_jtodevjirsd01_netinf" {
+        source = "github.com/cantrellcloud/tfaz-netinf"
+        #to_provision = local.provision_jtodevjirsd01_netinf
+        rg_location = module.jtodev_eastus2_dev_rg.rg_location
+        rg_name = module.jtodev_eastus2_dev_rg.rg_name
+    	rg_tags = {
+	    	"ManagementGroup" = "MPG A&E EastUS2",
+		    "Environment" = "JTODev",
+    		"AutomatedBy" = "Terraform",
+	    	"Note1" = "Do not manually change",
+		    "POCName" = "ronc@mindpointgroup.com",
+    		"POCPhone" = "843.330.6769",
+		    "Project" = "Zero Trust Demo"
+	    }
+
+        netinf_name = "jtodevjirsd01_netinf"
+
+        ip_configuration_name                          = "ipconfig01"
+        ip_configuration_private_ip_address_version    = "IPv4"
+        ip_configuration_subnet_id                     = module.jtodev_eastus2_dev_web_subnet.subnet_id
+        ip_configuration_private_ip_address_allocation = "Static"
+        ip_configuration_private_ip_address            = "172.16.200.60"
+        ip_configuration_public_ip_address_id          = null # module.jtodev_eastus2_dev_jtodevjirsd01_publicip.publicip_id
+}
+
+    # jtodevjirsd01_outputs
+    output "jtodev_eastus2_dev_jtodevjirsd01_vmwin_id" {
+        value = module.jtodev_eastus2_dev_jtodevjirsd01_vmwin.vmwin_id
+    }
+    output "jtodev_eastus2_dev_jtodevjirsd01_vmwin_name" {
+        value = module.jtodev_eastus2_dev_jtodevjirsd01_vmwin.vmwin_name
+    }
+    output "jtodev_eastus2_dev_jtodevjirsd01_vmwin_computer_name" {
+        value = module.jtodev_eastus2_dev_jtodevjirsd01_vmwin.vmwin_computer_name
+    }
+    output "jtodev_eastus2_dev_jtodevjirsd01_vmwin_private_ip_address" {
+        value = module.jtodev_eastus2_dev_jtodevjirsd01_vmwin.vmwin_private_ip_address
+    }
+    output "jtodev_eastus2_dev_jtodevjirsd01_vmwin_virtual_machine_id" {
+        value = module.jtodev_eastus2_dev_jtodevjirsd01_vmwin.vmwin_virtual_machine_id
+    }
+    output "jtodev_eastus2_dev_jtodevjirsd01_vmwin_public_ip_address" {
+        value = module.jtodev_eastus2_dev_jtodevjirsd01_vmwin.vmwin_public_ip_address
+    }
+
+    # jtodevjirsd01_netinf_outputs
+	output "jtodev_eastus2_dev_jtodevjirsd01_netinf_id" {
+		value = module.jtodev_eastus2_dev_jtodevjirsd01_netinf.netinf_id
+	}
+	output "jtodev_eastus2_dev_jtodevjirsd01_netinf_name" {
+		value = module.jtodev_eastus2_dev_jtodevjirsd01_netinf.netinf_name
+	}
+	output "jtodev_eastus2_dev_jtodevjirsd01_netinf_private_ip_address" {
+		value = module.jtodev_eastus2_dev_jtodevjirsd01_netinf.netinf_private_ip_address
+	}
+	output "jtodev_eastus2_dev_jtodevjirsd01_netinf_virtual_machine_id" {
+		value = module.jtodev_eastus2_dev_jtodevjirsd01_netinf.netinf_virtual_machine_id
+	}
+
+#########################################################################################
